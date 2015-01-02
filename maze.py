6dad44a08f22ecd792164e08e1c5db190ed16c37
Ozge Bascil <ozge14@gmail.com>
2015-01-02 00:05:59 +0000

    Created functions and constant values

---
+++ maze.py
@@ -0,0 +1,27 @@
+P = 'X'
+E = '.'
+W = '#'
+EX = 'O'
+
+
+def print_maze():
+    pass
+
+
+def get_input():
+    pass
+
+
+def update_maze():
+    pass
+
+
+def check_maze():
+    pass
+
+
+def run_game():
+    pass
+
+
+if __name__ == "__main__":
