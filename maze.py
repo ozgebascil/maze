7ab1170f9a849499d290ae3cc5d77a1df4397cf8
Ozge Bascil <ozge14@gmail.com>
2015-01-03 23:54:52 +0000

    Created get input function

--- maze.py
+++ maze.py
@@ -2,14 +2,25 @@ P = 'X'
 E = '.'
 W = '#'
 EX = 'O'
+R = 'R'
+L = 'L'
+U = 'U'
+D = 'D'
 
 
-def print_maze():
-    pass
+def print_maze(maze):
+    for row in maze:
+        print ' '.join(row)
 
 
 def get_input():
-    pass
+    direction = raw_input("Please enter direction l:left, r:right, u:up, d:down:")
+    direction = direction.upper().strip()
+    if direction not in [L, R, U, D]:
+        print "This is not direction!"
+        return get_input()
+    return direction
+
 
 
 def update_maze():
@@ -25,3 +36,11 @@ def run_game():
 
 
 if __name__ == "__main__":
+    #print_maze([[W, W, W, W, W, W],
+        #[W, E, E, E, P, W],
+        #[W, W, E, E, E, W],
+        #[W, E, E, W, W, W],
+        #[W, W, E, E, EX, W],
+        #[W, W, W, W, W, W]])
+
+    print get_input()
